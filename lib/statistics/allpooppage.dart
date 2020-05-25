16752778a7f6554eb34adf5dc807b491bb114c65
elisabet123 <elisabet.svensson@meltwater.com>
2020-05-25 18:42:51 +0200

    Removed duplicated code

--- lib/statistics/allpooppage.dart
+++ lib/statistics/allpooppage.dart
@@ -5,8 +5,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:intl/intl.dart';
 
-import '../database_helpers.dart';
-
 class AllPoopPage extends StatefulWidget {
   final int cutoffTime;
 
@@ -30,96 +28,9 @@ class AllPoopPageState extends PoopPageState {
     });
   }
 
-  Future<bool> confirmDelete(BuildContext context) async {
-    return await showDialog(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            title: Text(PoopLocalizations.of(context).get('remove_poop_title')),
-            content:
-                Text(PoopLocalizations.of(context).get('remove_poop_question')),
-            actions: <Widget>[
-              FlatButton(
-                  onPressed: () => Navigator.of(context).pop(true),
-                  child: Text(PoopLocalizations.of(context).get('remove'))),
-              FlatButton(
-                onPressed: () => Navigator.of(context).pop(false),
-                child: Text(PoopLocalizations.of(context).get('cancel')),
-              ),
-            ],
-          );
-        });
-  }
-
-  onDelete(BuildContext context, Poop poop) async {
-    super.deletePoop(poop);
-    scaffold.showSnackBar(SnackBar(
-        content: Text(
-            '${DateFormat('yyyy-MM-dd HH:mm').format(poop.dateTime)}' +
-                PoopLocalizations.of(context).get('removed'))));
-  }
-
   @override
   Widget build(BuildContext context) {
-    final Iterable<Dismissible> tiles = _poops.map(
-      (Poop poop) {
-        return Dismissible(
-          key: Key(poop.dateTime.millisecondsSinceEpoch.toString()),
-          onDismissed: (direction) => onDelete(context, poop),
-          child: ListTile(
-            title: Text(
-              '${DateFormat('yyyy-MM-dd HH:mm').format(poop.dateTime)}',
-            ),
-            trailing: Container(
-              child: Image(
-                image: poop.hardness != null
-                    ? AssetImage('assets/images/type-' +
-                        poop.hardness.floor().toString() +
-                        '.png')
-                    : AssetImage('assets/images/empty.png'),
-                height: 50,
-              ),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(5),
-                  border: Border.all(width: 0)),
-            ),
-            onLongPress: () async {
-              var delete = await confirmDelete(context);
-              if (delete) {
-                await onDelete(context, poop);
-              }
-            },
-          ),
-          background: Container(
-            alignment: AlignmentDirectional.centerEnd,
-            color: Colors.red,
-            child: Padding(
-              padding: EdgeInsets.fromLTRB(0.0, 0.0, 10.0, 0.0),
-              child: Icon(Icons.delete, color: Colors.white),
-            ),
-          ),
-          direction: DismissDirection.endToStart,
-          confirmDismiss: (DismissDirection direction) =>
-              confirmDelete(context),
-        );
-      },
-    );
-    final List<Widget> divided = ListTile.divideTiles(
-      context: context,
-      tiles: tiles,
-    ).toList();
-
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(PoopLocalizations.of(context).get('all_poops')),
-      ),
-      body: Builder(
-        builder: (BuildContext buildContext) {
-          scaffold = Scaffold.of(buildContext);
-          return ListView(children: divided);
-        },
-      ),
-    );
+    return AllPoopWidget(_poops, super.deletePoop);
   }
 }
 
@@ -130,7 +41,7 @@ class AllPoopWidget extends StatefulWidget {
   AllPoopWidget(this.poops, this._deletePoop, {Key key}) : super(key: key);
 
   @override
-  AllPoopWidgetState createState() => AllPoopWidgetState(poops, _deletePoop);
+  AllPoopWidgetState createState() => AllPoopWidgetState(this.poops, _deletePoop);
 }
 
 class AllPoopWidgetState extends State<AllPoopWidget> {
@@ -139,6 +50,14 @@ class AllPoopWidgetState extends State<AllPoopWidget> {
   ScaffoldState scaffold;
 
   AllPoopWidgetState(this.poops, this._deletePoop);
+
+  @override
+  void didUpdateWidget(Widget oldVariant) {
+    setState(() {
+      poops = widget.poops;
+    });
+    super.didUpdateWidget(oldVariant);
+  }
 
   Future<bool> confirmDelete(BuildContext context) async {
     return await showDialog(
